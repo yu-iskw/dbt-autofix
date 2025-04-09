c46b5e81cc8c10d5bd49e7dd7f621e988eecfcc7
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2025-04-09 14:13:17 +0200

    Update nox to install package and tests deps

--- noxfile.py
+++ noxfile.py
@@ -6,10 +6,12 @@ nox.options.default_venv_backend = "uv|venv"
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def run_cli(session):
     """Make sure the CLI runs correctly"""
+    session.install(".[test]")
     session.run("dbt-cleanup", "--help")
 
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def pytest(session):
     """Run the tests"""
+    session.install(".[test]")
     session.run("pytest")
