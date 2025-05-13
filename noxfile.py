8dbc3cc8b66281c6153356c4de36fa942acac1be
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2025-05-13 15:06:48 +0200

    Update tests

--- noxfile.py
+++ noxfile.py
@@ -3,14 +3,14 @@ import nox
 nox.options.default_venv_backend = "uv|venv"
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def run_cli(session):
     """Make sure the CLI runs correctly"""
-    session.install(".[test]")
+    session.install(".")
     session.run("dbt-autofix", "--help")
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def pytest(session):
     """Run the tests"""
     session.install(".[test]")
