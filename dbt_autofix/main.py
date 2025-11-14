6acdac0fe8e02d08885ce81437beaf5fc2660b33
Chaya <chaya.carey@dbtlabs.com>
2025-11-14 14:43:37 -0800

    Prototype package upgrade CLI option (#210)
    This change sets up a basic working prototype of a new CLI option to upgrade package dependencies. It currently only runs in dry run mode and does not pull upgraded packages from the registry. This is a WIP - more to come soon!
    
    * Identify and parse package dependency files
    
    * Parse package info and construct package versions from installed packages
    
    * Build versions for dependencies of packages installed and in dependencies
    
    * Output package upgrades to JSON

--- dbt_autofix/main.py
+++ dbt_autofix/main.py
@@ -11,6 +11,14 @@ from typing_extensions import Annotated
 from dbt_autofix.dbt_api import update_jobs
 from dbt_autofix.duplicate_keys import find_duplicate_keys, print_duplicate_keys
 from dbt_autofix.fields_properties_configs import print_matrix
+from dbt_autofix.package_upgrade import (
+    PackageUpgradeResult,
+    PackageVersionUpgradeResult,
+    check_for_package_upgrades,
+    generate_package_dependencies,
+    upgrade_package_versions,
+)
+from dbt_autofix.packages.dbt_package_file import DbtPackageFile
 from dbt_autofix.refactor import apply_changesets, changeset_all_sql_yml_files
 from dbt_autofix.retrieve_schemas import SchemaSpecs
 
@@ -35,6 +43,46 @@ def identify_duplicate_keys(
     print(f"[green]Identifying duplicates in {path}[/green]\n")
     project_duplicates, package_duplicates = find_duplicate_keys(path)
     print_duplicate_keys(project_duplicates, package_duplicates)
+
+
+@app.command(name="upgrade-packages")
+def upgrade_packages(
+    path: Annotated[Path, typer.Option("--path", "-p", help="The path to the dbt project")] = current_dir,
+    dry_run: Annotated[bool, typer.Option("--dry-run", "-d", help="In dry run mode, do not apply changes")] = False,
+    json_output: Annotated[bool, typer.Option("--json", "-j", help="Output in JSON format")] = False,
+):
+    # TODO: remove this when full package upgrade is available
+    print("[yellow]This command is still under development and will only operate in dry-run mode[/yellow]\n")
+    dry_run = True
+
+    print(f"[green]Identifying packages with available upgrades in {path}[/green]\n")
+    deps_file: Optional[DbtPackageFile] = generate_package_dependencies(path)
+    if not deps_file:
+        error_console.print("[red]-- No package dependency config found --[/red]")
+        return
+
+    if len(deps_file.package_dependencies) == 0:
+        error_console.print("[red]-- No package dependencies found --[/red]")
+        return
+
+    package_upgrades: list[PackageVersionUpgradeResult] = check_for_package_upgrades(deps_file)
+
+    if dry_run:
+        if not json_output:
+            error_console.print("[red]-- Dry run mode, not applying changes --[/red]")
+        upgrade_result = PackageUpgradeResult(
+            dry_run=True,
+            file_path=deps_file.file_path if deps_file.file_path else path,
+            upgraded=False,
+            upgrades=[],
+            unchanged=package_upgrades,
+        )
+        upgrade_result.print_to_console(json_output=True)
+    else:
+        upgrade_package_versions(package_upgrades, json_output)
+
+    if json_output:
+        print(json.dumps({"mode": "complete"}))  # noqa: T201
 
 
 @app.command(name="deprecations")
