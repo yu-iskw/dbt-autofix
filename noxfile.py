f8050d2b8144cc25b39e11fa6954bade57406509
Chaya <chaya.carey@dbtlabs.com>
2025-12-03 16:25:55 -0800

    Enable build for all packages and update nox to use uv backend (#247)
    * Enable build for all packages and update nox to use uv backend
    
    * Add GH action to run pytest on PRs
    
    * run pytest on multiple versions

--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,52 @@
 import nox
 
-nox.options.default_venv_backend = "uv|venv"
+nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"], venv_backend="uv")
 def run_cli(session):
     """Make sure the CLI runs correctly"""
-    session.install(".")
+    session.run_install(
+        "uv",
+        "sync",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("dbt-autofix", "--help")
 
 
-@nox.session(python=["3.13"])
+@nox.session(python=["3.13"], venv_backend="uv")
 def check_latest_schema(session):
     """Make sure the CLI runs correctly"""
-    session.install(".")
+    session.run_install(
+        "uv",
+        "sync",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("dbt-autofix", "print-fields-matrix")
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"], venv_backend="uv")
 def pytest(session):
     """Run the tests"""
-    session.install(".[test]")
-    session.run("pytest")
+    session.run_install(
+        "uv",
+        "sync",
+        "--extra=test",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("pytest", *session.posargs)
+
+
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"], venv_backend="uv")
+def run_cli_deprecations(session):
+    """Make sure the deperecations CLI runs (but fails)"""
+    session.run_install(
+        "uv",
+        "sync",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("dbt-autofix", "deprecations")
